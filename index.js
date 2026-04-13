56807ad34374a2efd1c5f95b04fbcc4c714e2a8b
MD.Shimul <md.shimuldiu@gmail.com>
2026-04-13 23:57:02 +0600

    setup typescript and first program

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+var username = 'shimul';
+var password = '234';
+console.log(username, password);
+function Addnumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+Addnumbers(10, 30);
---
+++ index.ts
@@ -0,0 +1,9 @@
+var username='shimul'
+var password:string='234'
+console.log(username,password)
+
+
+function Addnumbers(num1:number,num2:number){
+    console.log(num1+num2)
+}
+Addnumbers(10,30)
