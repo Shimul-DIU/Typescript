2a08f0b5ed03dd253a9fef9b1e65853486cfe888
MD.Shimul <md.shimuldiu@gmail.com>
2026-06-28 18:41:33 +0600

    refactor: reorganize code structure and enhance student object logging

--- index.js
+++ index.js
@@ -8,9 +8,26 @@ function Addnumbers(num1:number,num2:number){
     console.log(num1+num2)
 }
 Addnumbers(10,30) */
-let number = '564';
-console.log(number);
-function Multiplication(num1, num2) {
-    return num1 * num2;
+/*
+let number:string='564';
+console.log(number)
+
+function Multiplication(num1:number,num2:number):number{
+    return num1*num2
 }
-console.log(Multiplication(0, 9));
+
+console.log(Multiplication(0,9))
+ */
+let student = {
+    name: 'shimul',
+    roll: 8,
+    isStudent: true,
+    years: 40
+};
+// console.log(student)
+console.log(Object.entries(student));
+console.log(Object.keys(student));
+console.log(Object.values(student));
+Object.entries(student).forEach(([keys, values]) => {
+    console.log(`key is ${keys} and value is ${values}`);
+});
--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ function Addnumbers(num1:number,num2:number){
     console.log(num1+num2)
 }
 Addnumbers(10,30) */
-
+/*
 let number:string='564';
 console.log(number)
 
@@ -16,3 +16,18 @@ function Multiplication(num1:number,num2:number):number{
 }
 
 console.log(Multiplication(0,9))
+ */
+let student={
+    name:'shimul',
+    roll:8,
+    isStudent:true,
+    years:40
+}
+// console.log(student)
+console.log(Object.entries(student))
+console.log(Object.keys(student))
+console.log(Object.values(student))
+
+Object.entries(student).forEach(([keys,values])=>{
+    console.log(`key is ${keys} and value is ${values}`)
+})
